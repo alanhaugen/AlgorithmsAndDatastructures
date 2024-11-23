82f2a7ea9ac339debd46f42cbb682bd40c8ca724
Alan <alanhaugen@gmail.com>
2024-11-23 16:38:59 +0100

    Minimax is getting very close to being implemented

--- app/source/minimax.h
+++ app/source/minimax.h
@@ -10,10 +10,8 @@ class Minimax
 private:
     class GameState
     {
-    private:
-        Move move;
-
     public:
+        Move move;
         bool isMax;
         int nobility;
 
@@ -30,18 +28,21 @@ private:
     };
 
     Tree<GameState*> gameTree;
-    Tree<GameState>::Node *parentNode;
 
 public:
     Minimax();
+
+    Player* proponent;
+    Player* opponent;
 
     // To make a minimax tree, you need to use an algorithm called a heuristic algorithm
     // A heuristic is really just a fancy word that means general rule of thumb.
     // Different AIs for different games have different heuristic functions for different purposes.
     // The job of a heuristic function is to look at a move in a game and evaluate if it is a good move or a bad move.
     // Source: Ron Penton, Data Structures for Games Programmers (2003)
-    int CalculateMiniMaxValue(Board *gameBoard);
+    int CalculateMiniMaxValue(Board *gameBoard, Move move);
     Move FindBestMove(Player* min, Player* max, Board* gameBoard, int depth);
+    void AddLayer(Board* gameBoard, Tree<Minimax::GameState*>::Node* node, Player* player);
 };
 
 #endif // MINIMAX_H
